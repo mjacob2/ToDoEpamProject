0237a393271b0e67737599e425e64b266df9ec8f
Marek Jakubicki <jakubicki.m@gmail.com>
2023-02-10 22:21:06 +0100

    MediatR in request and response

---
+++ ToDoApp/ToDoEpam.ApplicationServices/API/Domain/Requests/GetToDoListsRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ToDoEpam.ApplicationServices.API.Domain.Responses;
+
+namespace ToDoEpam.ApplicationServices.API.Domain.Requests
+{
+        internal class GetToDoListsRequest : IRequest<GetToDoListsResponse>
+        {
+        }
+}
