9f718da7f39f6416d15e452b21670ab0cdb31bdb
Marek Jakubicki <jakubicki.m@gmail.com>
2023-02-10 22:18:04 +0100

    Install MediatR

---
+++ ToDoApp/ToDoApp/Controllers/ToDoListController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApp.Controllers
+{
+        [ApiController]
+        [Route("[controller]")]
+        public class ToDoListController : ControllerBase
+        {
+
+                public ToDoListController()
+                {
+
+                }
+
+        }
+}
